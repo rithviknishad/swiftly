e675e51f7a2e0437f5a7adefd4f2ddd29f986ad6
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-18 09:20:44 +0530

    AppRouter hook login and signup

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,19 @@
 import { useRoutes } from "raviger";
 import AppContainer from "../components/AppContainer";
+import Login from "../components/Login";
+import { Signup } from "../components/Signup";
+import { User } from "../types/UserTypes";
 
 const routes = {
   //   "/": () => <Home />,
-  //   "/login": () => <Login />,
-  //   "/signup": () => <SignUp />,
+  "/login": () => <Login />,
+  "/signup": () => <Signup />,
   //   "/boards": () => <Boards />,
   //   "/boards/:id": () => <KanbanBoardView />,
   //   "/todos/:id": () => <TodoView />,
 };
 
-export default function AppRouter() {
+export default function AppRouter(props: { currentUser: User | null }) {
   let routeResult = useRoutes(routes);
   return <AppContainer>{routeResult}</AppContainer>;
 }
