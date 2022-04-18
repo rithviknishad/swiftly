238b373e887349a75bc97c877a987142894eaa60
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-18 09:21:07 +0530

    Misc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AppRouter from "./router/AppRouter";
+import { User } from "./types/UserTypes";
+import { me } from "./utils/ApiUtils";
+
+const getCurrentUser = async (setCurrentUser: (user: User) => void) =>
+  setCurrentUser(await me());
 
 export default function App() {
-  return <AppRouter />;
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getCurrentUser(setCurrentUser);
+  }, []);
+
+  return <AppRouter currentUser={currentUser} />;
 }
--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function AppContainer(props: { children: React.ReactNode }) {
-  return <div>{props.children}</div>;
+  return <div className="bg-[#F7F7F7]">{props.children}</div>;
 }
