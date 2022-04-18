89e7caa9875c9e642d44c6a529a3df96ecd3aa4d
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-18 09:19:33 +0530

    Adds Common Types

---
+++ src/types/CommonTypes.tsx
@@ -0,0 +1,19 @@
+export type Errors<T> = Partial<Record<keyof T, string>>;
+
+export type Paginated<T> = {
+  count: number;
+  previous: string | null;
+  next: string | null;
+  results: T[];
+};
+
+export type PaginationParams = {
+  limit: number;
+  offset: number;
+};
+
+export type ModelProps = {
+  id?: Readonly<number>;
+  created_date?: Readonly<number>;
+  modified_date?: Readonly<number>;
+};
