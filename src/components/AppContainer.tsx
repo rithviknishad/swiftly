6d4e60253eb3b66cb406476cf089af95f2583ba9
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-19 21:58:09 +0530

    Adds theme toggle

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function AppContainer(props: { children: React.ReactNode }) {
-  return <div className="bg-white">{props.children}</div>;
+  return (
+    <div className="bg-white dark:bg-gray-900 transition-all">
+      {props.children}
+    </div>
+  );
 }
