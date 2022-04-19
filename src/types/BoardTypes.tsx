592c9190cf2a274612ef3f7de8c0641abf547eca
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-19 16:22:13 +0530

    validate board

--- src/types/BoardTypes.tsx
+++ src/types/BoardTypes.tsx
@@ -1,19 +1,32 @@
-import { ModelProps } from "./CommonTypes";
+import { Errors } from "./DashboardPageTypes";
 
-export type Board = ModelProps & {
+export type Board = {
   title: string;
   description: string;
 };
 
-export type Status = ModelProps & {
+export type Status = {
   title: string;
   description: string;
   board: number;
 };
 
-export type Task = ModelProps & {
+export type Task = {
   status: number;
   board: number;
   title: string;
   description: string;
+};
+
+export const validateBoard = (board: Board) => {
+  const errors: Errors<Board> = {};
+
+  if (board.title.length < 1) {
+    errors.title = "Title is required";
+  }
+  if (board.title.length > 100) {
+    errors.title = "Title must be less than 100 characters";
+  }
+
+  return errors;
 };
