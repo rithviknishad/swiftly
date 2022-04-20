ce82a1b29ed054cdd7b386b78f7093ab81a5d00a
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-20 22:37:36 +0530

    chore

--- src/components/KanbanBoardView.tsx
+++ src/components/KanbanBoardView.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer, useState } from "react";
 import { Board } from "../types/BoardTypes";
 import { Model } from "../types/CommonTypes";
 import { updateBoard } from "../utils/ApiUtils";
+import { getLocalBoards, updateLocalBoard } from "../utils/StorageUtils";
 import Modal from "./commons/Modal";
 import CreateStatus from "./CreateStatus";
 import DashboardBase from "./DashboardBase";
@@ -28,16 +29,24 @@ const boardReducer = (state: Model<Board>, action: BoardAction) => {
   return state;
 };
 
-export function KanbanBoardView(props: { board: Model<Board> }) {
-  const [board, dispatch] = useReducer(boardReducer, null, () => props.board);
+export function KanbanBoardView(props: { boardId: number }) {
+  const [board, dispatchBoardAction] = useReducer(
+    boardReducer,
+    null,
+    () => getLocalBoards().find((b) => b.id === props.boardId)!
+  );
   const [title, setTitle] = useState(() => board.title);
 
   const [newTask, setNewTask] = useState(false);
   const [newStatus, setNewStatus] = useState(false);
 
   useEffect(() => {
+    updateLocalBoard(board);
+  }, [board]);
+
+  useEffect(() => {
     const timeoutId = setTimeout(() => {
-      dispatch({ type: "edit_board_title", title: title });
+      dispatchBoardAction({ type: "edit_board_title", title: title });
     }, 1000);
 
     return () => {
@@ -48,18 +57,17 @@ export function KanbanBoardView(props: { board: Model<Board> }) {
   return (
     <DashboardBase selectedTabName="Boards">
       <div className="p-6">
-        <div className="flex flex-row items-center">
+        <div className="flex flex-row items-center gap-6">
           <input
             type="text"
             name="title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className="text-4xl font-medium w-full rounded-lg p-2 my-2 bg-transparent flex-1 focus:bg-gray-50 hover:bg-gray-50 focus:dark:bg-gray-800 hover:dark:bg-gray-800"
+            className="flex-1 text-4xl font-medium w-full rounded-lg p-2 my-2 bg-transparent focus:bg-gray-50 hover:bg-gray-50 focus:dark:bg-gray-800 hover:dark:bg-gray-800"
           />
-          <div className="flex-1"></div>
           <button
             onClick={(e) => setNewStatus(true)}
-            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+            className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
           >
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               New Status
@@ -82,7 +90,10 @@ export function KanbanBoardView(props: { board: Model<Board> }) {
           </div>
         )} */}
         <Modal open={newStatus} closeCB={() => setNewStatus(false)}>
-          <CreateStatus boardId={props.board.id!} />
+          <CreateStatus
+            boardId={props.boardId}
+            closeCB={() => setNewStatus(false)}
+          />
         </Modal>
       </div>
     </DashboardBase>
