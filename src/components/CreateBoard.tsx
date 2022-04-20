2a3bf7ff75dccaa420367e4618f59a058ae44336
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-20 22:37:27 +0530

    chore

--- src/components/CreateBoard.tsx
+++ src/components/CreateBoard.tsx
@@ -63,7 +63,7 @@ export default function CreateBoard() {
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Submit
+          Create Board
         </button>
       </form>
     </div>
