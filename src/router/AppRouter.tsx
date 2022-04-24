0e5b549938fc22d609b8a138ca79db32f4eab19a
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-24 10:30:14 +0530

    Lazy Loading

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,18 +1,29 @@
 import { Redirect, useRoutes } from "raviger";
+import React, { ReactNode, Suspense } from "react";
 import AppContainer from "../components/AppContainer";
-import Boards from "../components/Boards";
-import Home from "../components/Home";
-import { KanbanBoardView } from "../components/KanbanBoardView";
-import Login from "../components/Login";
-import Signup from "../components/Signup";
 import { User } from "../types/UserTypes";
 
+const Signup = React.lazy(() => import("../components/Signup"));
+const Login = React.lazy(() => import("../components/Login"));
+const Home = React.lazy(() => import("../components/Home"));
+const Boards = React.lazy(() => import("../components/Boards"));
+const KanbanBoardView = React.lazy(
+  () => import("../components/KanbanBoardView")
+);
+
+function LazyLoaded(props: { child: ReactNode }) {
+  return <Suspense fallback={<div>fetching...</div>}>{props.child}</Suspense>;
+}
+
 const routes = {
-  "/": () => <Home />,
-  "/login": () => <Login />,
-  "/signup": () => <Signup />,
+  // Redirects
   "/home": () => <Redirect to="/" />,
-  "/boards": () => <Boards />,
+
+  // Routes
+  "/": () => <LazyLoaded child={<Home />} />,
+  "/login": () => <LazyLoaded child={<Login />} />,
+  "/signup": () => <LazyLoaded child={<Signup />} />,
+  "/boards": () => <LazyLoaded child={<Boards />} />,
   "/boards/:id": ({ id }: { id: string }) => (
     <KanbanBoardView boardId={Number(id)} />
   ),
