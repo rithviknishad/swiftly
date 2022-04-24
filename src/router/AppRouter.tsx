0c5f76538fdd16aad77ddd0498f34117be69c04f
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-24 10:33:22 +0530

    revert lazy loading

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,7 +2,7 @@ import { Redirect, useRoutes } from "raviger";
 import AppContainer from "../components/AppContainer";
 import Boards from "../components/Boards";
 import Home from "../components/Home";
-import { KanbanBoardView } from "../components/KanbanBoardView";
+import KanbanBoardView from "../components/KanbanBoardView";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
 import { User } from "../types/UserTypes";
